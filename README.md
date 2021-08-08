5135bc1c730483f3a40d3f51beb543ac21733d3a
alexkariuki <alexkariuki2@gmail.com>
2021-08-08 08:49:50 +0300

    Editting readme

--- README.md
+++ README.md
@@ -1,27 +1,24 @@
-# EmsWeb
+# Emergency Responce Platform (First Response)
 
-This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 12.1.3.
+This project is geared to provide a platform where users can call into an emergency hotline and have their emergency call answered as well as a responce team sent to investigate the matter. The process pipeline looks something like the image below:
 
-## Development server
+## Draft Architecture
 
-Run `ng serve` for a dev server. Navigate to `http://localhost:4200/`. The app will automatically reload if you change any of the source files.
 
-## Code scaffolding
 
-Run `ng generate component component-name` to generate a new component. You can also use `ng generate directive|pipe|service|class|guard|interface|enum|module`.
+## Draft UI design
 
-## Build
+The UI design of the project is inspired by [Trevor Lang](https://dribbble.com/shots/6651328-E84-Headlamp/attachments/1421261?mode=media).
+
+## Services Offered
 
 Run `ng build` to build the project. The build artifacts will be stored in the `dist/` directory.
 
-## Running unit tests
+## Building the project
 
-Run `ng test` to execute the unit tests via [Karma](https://karma-runner.github.io).
+Run `ng build` to build the project. The build artifacts will be stored in the `dist/` directory.
 
 ## Running end-to-end tests
 
 Run `ng e2e` to execute the end-to-end tests via a platform of your choice. To use this command, you need to first add a package that implements end-to-end testing capabilities.
 
-## Further help
-
-To get more help on the Angular CLI use `ng help` or go check out the [Angular CLI Overview and Command Reference](https://angular.io/cli) page.
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,7 +8,7 @@ import { environment } from '../environments/environment';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
- 
+
   title = 'ems-web';
 
   lat = -1.239362;
@@ -17,18 +17,45 @@ export class AppComponent {
   map!: mapboxgl.Map;
   @ViewChild('mapElement')
   mapElement!: ElementRef;
-  
+
 
   currentdate: any;
- datetime: any;
+  datetime: any;
+geojson: any;
 
+  
 
   constructor() {
     /*Initializing Map*/
-    
+    this.geojson = [
+      {
+        type: 'Feature',
+        geometry: {
+          type: 'Point',
+          coordinates: [-77.031952, 38.913184]
+        },
+        properties: {
+          'marker-color': '#3bb2d0',
+          'marker-size': 'large',
+          'marker-symbol': 'rocket'
+        }
+      },
+      {
+        type: 'Feature',
+        geometry: {
+          type: 'Point',
+          coordinates: [-122.413682, 37.775408]
+        },
+        properties: {
+          'marker-color': '#3bb2d0',
+          'marker-size': 'large',
+          'marker-symbol': 'rocket'
+        }
+      }
+    ];
   }
   ngOnInit() {
-    (mapboxgl as any).accessToken =  environment.mapboxToken;
+    (mapboxgl as any).accessToken = environment.mapboxToken;
 
     this.currentdate = new Date();
     this.datetime = "Last Sync: " +
@@ -42,11 +69,27 @@ export class AppComponent {
     this.map = new mapboxgl.Map({
       container: this.mapElement.nativeElement,
       style: 'mapbox://styles/mapbox/streets-v9',
-      zoom: 13,
+      zoom: 12,
       center: [this.lng, this.lat],
-      maxZoom:100,
-      scrollZoom: true
-    });
+      maxZoom: 100,
+      scrollZoom: false
+    }).addControl(new mapboxgl.AttributionControl({
+      customAttribution: 'Map design by me'
+    }));
+
+    this.addMarker()
+
+  }
+
+  addMarker() {
+    const marker = new mapboxgl.Marker({
+      color: "#FFFFFF",
+    
+      draggable: true,
+    }).setLngLat([this.lng, this.lat])
+      .setPopup(new mapboxgl.Popup({ offset: 25 }) // add popups
+        .setHTML('<h6>Camp Fire</h6><p>Description</p>'))
+      .addTo(this.map);
   }
 
 }
